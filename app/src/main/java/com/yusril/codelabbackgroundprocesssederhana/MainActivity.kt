ed6840015592cda75930804e69ff157a2979485d
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-03-16 22:49:50 +0700

    Executor & Handler

--- app/src/main/java/com/yusril/codelabbackgroundprocesssederhana/MainActivity.kt
+++ app/src/main/java/com/yusril/codelabbackgroundprocesssederhana/MainActivity.kt
@@ -2,8 +2,11 @@ package com.yusril.codelabbackgroundprocesssederhana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,22 +14,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
-
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
         btnStart.setOnClickListener {
+            executor.execute{
             try {
                 for(i in 0..10){
                     Thread.sleep(500)
                     val percentage=i*10
-                    if(percentage==100){
-                        tvStatus.setText(R.string.task_completed)
+                    handler.post {
+                        if(percentage==100){
+                            tvStatus.setText(R.string.task_completed)
+                        }
+                        else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                        }
                     }
-                    else {
-                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                    }
+
                 }
             }catch (e: InterruptedException) {
                 e.printStackTrace()
             }
+        }
         }
     }
 }
